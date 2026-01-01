bff607959c8fb32fd85e7ce6c0a8cff60c54ed67
Albin Bicaj <bicajalbin@gmail.com>
2026-01-01 23:50:34 +0100

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
